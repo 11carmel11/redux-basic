24866dc13bd73c2c257ed7c7b3fa5ab0bc4ff18e
11carmel11 <11carmrl11@gmail.com>
2021-12-20 14:25:03 +0200

    converted to temporary DB

--- 29.redux/redux-shopcart/src/App.js
+++ 29.redux/redux-shopcart/src/App.js
@@ -5,26 +5,27 @@ import Cart from "./components/Cart";
 // import { Counter } from "./features/counter/Counter";
 // <Counter />;
 
-const products = [
-  { name: "banana", price: 5, amount: 8 },
-  { name: "apple", price: 3, amount: 12 },
-  { name: "tomato", price: 2.5, amount: 9 },
-  { name: "cucumber", price: 4, amount: 10 },
-];
-
-const cart = [
-  { name: "banana", price: 5, amount: 2 },
-  { name: "apple", price: 3, amount: 0 },
-  { name: "tomato", price: 2.5, amount: 1 },
-  { name: "cucumber", price: 4, amount: 5 },
-];
+const DB = {
+  products: [
+    { name: "banana", price: 5, amount: 8 },
+    { name: "apple", price: 3, amount: 12 },
+    { name: "tomato", price: 2.5, amount: 9 },
+    { name: "cucumber", price: 4, amount: 10 },
+  ],
+  cart: [
+    { name: "banana", price: 5, amount: 2 },
+    { name: "apple", price: 3, amount: 0 },
+    { name: "tomato", price: 2.5, amount: 1 },
+    { name: "cucumber", price: 4, amount: 5 },
+  ],
+};
 
 function App() {
   return (
     <>
       <Header />
-      <ShopList products={products} />
-      <Cart cart={cart} checkoutHandler={() => {}} />
+      <ShopList products={DB.products} />
+      <Cart cart={DB.cart} checkoutHandler={() => {}} />
     </>
   );
 }
