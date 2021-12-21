ffbdbb7a755d9dce213bf2e5041aea9f229a5809
11carmel11 <11carmrl11@gmail.com>
2021-12-21 16:54:18 +0200

    added nanoid for keys

--- 30.react-useReducer/task/src/components/FriendsList.jsx
+++ 30.react-useReducer/task/src/components/FriendsList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { nanoid } from "nanoid";
 import Friend from "./Friend";
 import friendsContext from "../contexts/friends/context";
 import { useContext } from "react";
@@ -17,29 +18,8 @@ export default function FriendsList() {
         </tr>
       </thead>
       {friends.map((friend, i) => (
-        <Friend key={i + friend.name} friend={friend} index={i} />
+        <Friend key={nanoid()} friend={friend} index={i} />
       ))}
     </Table>
   );
 }
-
-/**
- * <Table striped bordered hover>
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-  </tbody>
-</Table>
- */
