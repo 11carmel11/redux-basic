4bb1a95a99a87fd3624080d8f8ee8ae26470b115
11carmel11 <11carmrl11@gmail.com>
2021-12-21 14:13:12 +0200

    changed friends
    changed names to VS-code suggestions to avoid warnings

--- 30.react-useReducer/task/src/DB/db.js
+++ 30.react-useReducer/task/src/DB/db.js
@@ -1,10 +1,10 @@
 exports.db = [
   { name: "Vi", age: 24 },
   { name: "Jinx", age: 17 },
-  { name: "Ekko", age: 16 },
-  { name: "Caitlyn", age: 25 },
-  { name: "Heimerdinger", age: 317 },
-  { name: "Vander", age: 44 },
-  { name: "Silco", age: 43 },
-  { name: "Jayce", age: 34 },
+  { name: "Dekko", age: 16 },
+  { name: "Caitlin", age: 25 },
+  { name: "Heifer-dingier", age: 317 },
+  { name: "Vader", age: 44 },
+  { name: "Wilco", age: 43 },
+  { name: "Jaycee", age: 34 },
 ];
