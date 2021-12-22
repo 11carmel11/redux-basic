4c01472c87586c0f9ecdfdfc5fdad513ce3b1754
11carmel11 <11carmrl11@gmail.com>
2021-12-22 19:02:43 +0200

    removed loader

--- 31.redux-thunk/src/App.js
+++ 31.redux-thunk/src/App.js
@@ -4,7 +4,6 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import Select from "./components/Select";
 import Ticket from "./components/Ticket";
-// import Loader from "./components/Loader";
 import weatherReducer from "./reducers/weatherReducer";
 
 const store = createStore(weatherReducer, {}, applyMiddleware(thunk));
